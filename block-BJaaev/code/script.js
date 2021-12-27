7fdac0ffaaac6f573f0cdce0858693f5258a34ca
ravindrapv <ravindra.ravi4849@gmail.com>
2021-12-28 00:59:02 +0530

    add todo list

--- block-BJaaev/code/script.js
+++ block-BJaaev/code/script.js
@@ -1,38 +1,59 @@
-let inputtext = document.querySelector('#text');
-let root = document.querySelector('ul');
+function main(){
+    let inputtext = document.querySelector("#text");
+let root = document.querySelector("ul");
 
-let allTodos = [{},{}]
+let allTodos = [{}, {}];
 
-function handlinput(event){
-    if(event.keycode === 13 &&
-     event.target.value !== "");
-    let todo = {
-        name: event.target.value,
-        isDone: false
-    };
-    allTodos.push(todo);
-    event.target.value = "";
+function handlinput(event) {
+  if (event.keyode === 13 && 
+    event.target.value !== "");
+  let todo = {
+    name: event.target.value,
+    isDone: false,
+  };
+  allTodos.push(todo);
+  event.target.value = "";
 
-
-    createUI()
+  createUI();
 }
 
-inputtext.addEventListener("keyup",handlinput);
+function handleDelet(event){
+   let id = event.target.dataset.id
 
+   allTodos.splice(id,1);
+   createUI()
+}
 
-function createUI(){
-    root.innerHTML = "";
-   allTodos.forEach((todo) => {
-    let li = document.createElement('li');
-    let input = document.createElement('input');
+function hanleTogale(event) {
+   let id = event.target.dataset.id;
+   allTodos[id].isDone = !allTodos[id].isDone;
+
+   createUI();
+}
+
+function createUI() {
+  root.innerHTML = "";
+  allTodos.forEach((todo,index) => {
+    let li = document.createElement("li");
+    li.classList.add("flex");
+    let input = document.createElement("input");
     input.type = "checkbox";
     input.checked = todo.isDone;
-    let p = document.createElement('p');
-    p.innerText = todo.name
-    let span = document.createElement('span');
-    span.innerText = "X"
-    li.append(input,p,span);
-   root.append(li)
-   });
+    input.setAttribute('data-id', index)
+    input.addEventListener('input', hanleTogale)
+    let p = document.createElement("p");
+    p.innerText = todo.name;
+    let span = document.createElement("span");
+    span.innerText = "X";
+    li.append(input, p, span);
+    root.append(li);
+
+
+    span.addEventListener("click",handleDelet)
+    span.setAttribute("data-id" , index)
+  });
 }
-createUI();
+inputtext.addEventListener("keyup", handlinput);
+}
+
+main();
